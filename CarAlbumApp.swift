82d7fec1da93d31391abbc092e17cdd4efe98a5e
Yuno02y <126051685+Yuno02y@users.noreply.github.com>
2026-01-08 13:59:31 +0900

    Add files via upload

---
+++ CarAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAlbumApp.swift
+//  CarAlbum
+//
+//  Created by K on 2026/01/08.
+//
+
+import SwiftUI
+
+@main
+struct CarAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
